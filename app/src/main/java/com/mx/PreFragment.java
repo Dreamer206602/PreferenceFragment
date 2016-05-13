1277d3704e738c2b3c5abc35be6963c532507243
Dreamer206602 <boobooMX@163.com>
2016-05-13 22:39:03 +0800

    添加部分测试

--- app/src/main/java/com/mx/PreFragment.java
+++ app/src/main/java/com/mx/PreFragment.java
@@ -2,11 +2,13 @@ package com.mx;
 
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 /**
@@ -25,6 +27,24 @@ public class PreFragment extends PreferenceFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
        addPreferencesFromResource(R.xml.preferences);
+
+        findPreference(getString(R.string.checkbox_preference)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Toast.makeText(getActivity(),"CheckBox",Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        findPreference(getString(R.string.list_preference)).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Toast.makeText(getActivity(),"ListView",Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+
 
     }
 }
